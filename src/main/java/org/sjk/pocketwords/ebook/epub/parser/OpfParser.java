2baaf306718c11bb44b4adc12ddb5f757f613962
Simon Josef Kreuzpointner <simonkreuzpointner@gmail.com>
2026-05-17 21:31:45 +0200

    refactor: extract logic for parsing certain elements into separate methods to reduce complexity.

--- src/main/java/org/sjk/pocketwords/ebook/epub/parser/OpfParser.java
+++ src/main/java/org/sjk/pocketwords/ebook/epub/parser/OpfParser.java
@@ -42,6 +42,94 @@ public class OpfParser implements Parser<Opf, Path> {
     private static final String ITEM_REF_ELEMENT_NAME = "itemref";
     private static final String ID_REF_ATTRIBUTE_NAME = "idref";
 
+    private static void parseMetadataElement(final XMLEventReader reader, final OpfImpl.Builder builder)
+        throws XMLStreamException {
+        final List<String> authors = new ArrayList<>();
+        while (reader.hasNext()) {
+            final XMLEvent event = reader.nextEvent();
+            if (event.isStartElement()) {
+                final StartElement startElement = event.asStartElement();
+                final String startElementName = startElement.getName().getLocalPart();
+                switch (startElementName) {
+                    case IDENTIFIER_ELEMENT_NAME -> {
+                        final String identifier = reader.nextEvent().asCharacters().getData();
+                        builder.identifier(identifier);
+                    }
+                    case TITLE_ELEMENT_NAME -> {
+                        final String title = reader.nextEvent().asCharacters().getData();
+                        builder.title(title);
+                    }
+                    case CREATOR_ELEMENT_NAME -> {
+                        final String author = reader.nextEvent().asCharacters().getData();
+                        authors.add(author);
+                    }
+                }
+            } else if (event.isEndElement()) {
+                final EndElement endElement = event.asEndElement();
+                final String endElementName = endElement.getName().getLocalPart();
+                if (endElementName.equals(METADATA_ELEMENT_NAME)) {
+                    // done with parsing the <metadata> block
+                    builder.authors(authors);
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void parseSpineElement(final XMLEventReader reader,
+                                          final OpfImpl.Builder builder,
+                                          final Map<String, String> items
+    ) throws XMLStreamException {
+        final List<String> chapters = new ArrayList<>();
+        while (reader.hasNext()) {
+            final XMLEvent event = reader.nextEvent();
+            if (event.isStartElement()) {
+                final StartElement startElement = event.asStartElement();
+                final String startElementName = startElement.getName().getLocalPart();
+                if (startElementName.equals(ITEM_REF_ELEMENT_NAME)) {
+                    // parse <itemref> element
+                    final String idRef = startElement.getAttributeByName(new QName(ID_REF_ATTRIBUTE_NAME)).getValue();
+                    // FIXME: quick and dirty fix to only get text chapter entries
+                    if (idRef.startsWith("id-")) {
+                        chapters.add(items.get(idRef));
+                    }
+                }
+            } else if (event.isEndElement()) {
+                final EndElement endElement = event.asEndElement();
+                final String endElementName = endElement.getName().getLocalPart();
+                if (endElementName.equals(SPINE_ELEMENT_NAME)) {
+                    // done with parsing the <spine> block
+                    builder.chapterFiles(chapters.stream().map(Path::of).toList());
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void parseManifestElement(final XMLEventReader reader, final Map<String, String> items)
+        throws XMLStreamException {
+        while (reader.hasNext()) {
+            final XMLEvent event = reader.nextEvent();
+            if (event.isStartElement()) {
+                final StartElement startElement = event.asStartElement();
+                final String startElementName = startElement.getName().getLocalPart();
+                if (startElementName.equals(ITEM_ELEMENT_NAME)) {
+                    // parse <item> element
+                    final String id = startElement.getAttributeByName(new QName(ID_ATTRIBUTE_NAME)).getValue();
+                    final String href = startElement.getAttributeByName(new QName(HREF_ATTRIBUTE_NAME)).getValue();
+                    items.put(id, href);
+                }
+            } else if (event.isEndElement()) {
+                final EndElement endElement = event.asEndElement();
+                final String endElementName = endElement.getName().getLocalPart();
+                if (endElementName.equals(MANIFEST_ELEMENT_NAME)) {
+                    // done with parsing the <manifest> block
+                    break;
+                }
+            }
+        }
+    }
+
     @Override
     public Opf parse(final Path input) throws ParsingException {
         try {
@@ -65,102 +153,16 @@ public class OpfParser implements Parser<Opf, Path> {
     }
 
     private void readFile(final XMLEventReader reader, final OpfImpl.Builder builder) throws XMLStreamException {
-        final List<String> authors = new ArrayList<>();
         final Map<String, String> items = new HashMap<>();
-        final List<String> chapters = new ArrayList<>();
-
         while (reader.hasNext()) {
             final XMLEvent event = reader.nextEvent();
             if (event.isStartElement()) {
                 final StartElement startElement = event.asStartElement();
                 final String startElementName = startElement.getName().getLocalPart();
                 switch (startElementName) {
-                    case METADATA_ELEMENT_NAME -> {
-                        // parse <metadata> block
-                        while (reader.hasNext()) {
-                            final XMLEvent event2 = reader.nextEvent();
-                            if (event2.isStartElement()) {
-                                final StartElement startElement2 = event2.asStartElement();
-                                final String startElement2Name = startElement2.getName().getLocalPart();
-                                switch (startElement2Name) {
-                                    case IDENTIFIER_ELEMENT_NAME -> {
-                                        final String identifier = reader.nextEvent().asCharacters().getData();
-                                        builder.identifier(identifier);
-                                    }
-                                    case TITLE_ELEMENT_NAME -> {
-                                        final String title = reader.nextEvent().asCharacters().getData();
-                                        builder.title(title);
-                                    }
-                                    case CREATOR_ELEMENT_NAME -> {
-                                        final String author = reader.nextEvent().asCharacters().getData();
-                                        authors.add(author);
-                                    }
-                                }
-                            } else if (event2.isEndElement()) {
-                                final EndElement endElement2 = event2.asEndElement();
-                                final String endElement2Name = endElement2.getName().getLocalPart();
-                                if (endElement2Name.equals(METADATA_ELEMENT_NAME)) {
-                                    // done with parsing the <metadata> block
-                                    builder.authors(authors);
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    case MANIFEST_ELEMENT_NAME -> {
-                        // parse <manifest> block
-                        while (reader.hasNext()) {
-                            final XMLEvent event3 = reader.nextEvent();
-                            if (event3.isStartElement()) {
-                                final StartElement startElement3 = event3.asStartElement();
-                                final String startElement3Name = startElement3.getName().getLocalPart();
-                                if (startElement3Name.equals(ITEM_ELEMENT_NAME)) {
-                                    // parse <item> element
-                                    final String id =
-                                        startElement3.getAttributeByName(new QName(ID_ATTRIBUTE_NAME)).getValue();
-                                    final String href =
-                                        startElement3.getAttributeByName(new QName(HREF_ATTRIBUTE_NAME)).getValue();
-                                    items.put(id, href);
-                                }
-                            } else if (event3.isEndElement()) {
-                                final EndElement endElement3 = event3.asEndElement();
-                                final String endElement3Name = endElement3.getName().getLocalPart();
-                                if (endElement3Name.equals(MANIFEST_ELEMENT_NAME)) {
-                                    // done with parsing the <manifest> block
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    case SPINE_ELEMENT_NAME -> {
-                        // parse <spine> block
-                        while (reader.hasNext()) {
-                            final XMLEvent event4 = reader.nextEvent();
-                            if (event4.isStartElement()) {
-                                final StartElement startElement4 = event4.asStartElement();
-                                final String startElement4Name = startElement4.getName().getLocalPart();
-                                if (startElement4Name.equals(ITEM_REF_ELEMENT_NAME)) {
-                                    // parse <itemref> element
-                                    final String idRef =
-                                        startElement4.getAttributeByName(new QName(ID_REF_ATTRIBUTE_NAME)).getValue();
-                                    // FIXME: quick and dirty fix to only get text chapter entries
-                                    if (idRef.startsWith("id-")) {
-                                        chapters.add(items.get(idRef));
-                                    }
-                                }
-                            } else if (event4.isEndElement()) {
-                                final EndElement endElement4 = event4.asEndElement();
-                                final String endElement4Name = endElement4.getName().getLocalPart();
-                                if (endElement4Name.equals(SPINE_ELEMENT_NAME)) {
-                                    // done with parsing the <spine> block
-                                    builder.chapterFiles(chapters.stream()
-                                                                 .map(Path::of)
-                                                                 .toList());
-                                    break;
-                                }
-                            }
-                        }
-                    }
+                    case METADATA_ELEMENT_NAME -> OpfParser.parseMetadataElement(reader, builder);
+                    case MANIFEST_ELEMENT_NAME -> OpfParser.parseManifestElement(reader, items);
+                    case SPINE_ELEMENT_NAME -> OpfParser.parseSpineElement(reader, builder, items);
                 }
             }
         }
